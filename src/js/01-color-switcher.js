80455f626201c7c30b39c1aa9a7ba8a8618af972
Rummut <kzantip@gmail.com>
2023-06-28 00:51:39 +0300

    find an error

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,6 +8,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+let colorSwitcherId = null
 
 startBtnEl.addEventListener("click", () => {
     colorSwitcherId = setInterval(() => { bodyEl.style.backgroundColor = getRandomHexColor() }, 1000)
