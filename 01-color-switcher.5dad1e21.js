a6b33b707ecdc7996112ed18e683bfcd6e5e08b4
Rummut <123414878+Rummut@users.noreply.github.com>
2023-06-27 21:37:30 +0000

    Deploying to gh-pages from @ Rummut/goit-js-hw-09@7c6743234039fe8ace4031381fe8ff91829fad07 🚀

---
+++ 01-color-switcher.5dad1e21.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(()=>{colorSwitcherId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(colorSwitcherId),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.5dad1e21.js.map
